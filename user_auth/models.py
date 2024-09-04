8cbbb205d0849abb82f5d5e72e5daba63ad5aa92
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-09-04 00:09:30 -0300

    feat: create user_auth models

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class User_Auth(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    fullname = models.CharField(max_length=255)
+    weight = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.password = make_password(self.password)
+        super(User_Auth, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.fullname
